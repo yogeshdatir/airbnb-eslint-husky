229cb3c83c69f3b24fb93ccc95aca60fc2849b9e
Yogesh Datir <yogdat@gmail.com>
2023-08-12 09:54:24 +0530

    eslint added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,35 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  ignorePatterns: ['*.config.[jt]s'],
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    // Note: you must disable the base rule as it can report incorrect errors
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'linebreak-style': ['error', 'windows'],
+  },
+};
